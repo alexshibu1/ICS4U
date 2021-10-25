5d21bf1fc4400ff3bb2764efe06bf3ff187129c4
Alex Shibu <44331371+alexshibu1@users.noreply.github.com>
2021-10-25 13:50:51 -0400

    Add files via upload

---
+++ main (3).py
@@ -0,0 +1,21 @@
+print(
+'''
+Name: Alex Shibu 
+Grade: 12
+Descripion: A program orders three integers from lowest to highest 
+Date: October 12, 2021\n
+''')
+
+zero_list = [0,0,0,0,0,0,0]
+one_list = [1,1,1,1,1,1]
+
+print(zero_list + one_list)
+print(zero_list*3)
+print(one_list + zero_list)
+print(zero_list + [2,2,2])
+
+
+for i in zero_list:
+  print(i)
+for i in one_list:
+  print(i)
